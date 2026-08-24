cc5ee29970115cb8bdb2aa6c044c2d8f835bb6c6
sahilchainani08 <sahilchainani08@gmail.com>
2026-08-24 08:02:19 +0530

    new songs to car

--- src/data/carplaylist.js
+++ src/data/carplaylist.js
@@ -5,13 +5,44 @@ const carPlaylist = [
     artist: "Après Minuit",
     audioUrl: "https://res.cloudinary.com/drkkzlx2q/video/upload/v1787335635/Temperature.mp3",
   },
-
-    {
+  {
     id: 2,
+    title: "Belong Together",
+    artist: "Mark Ambor",
+    audioUrl: "https://res.cloudinary.com/drkkzlx2q/video/upload/v1786467908/Mark_Ambor_-_Belong_Together_Official_Video.mp3",
+  },
+{
+    id: 3,
+    title: "I don't care",
+    artist: "Ed sheeran x Justin Bieber",
+    audioUrl: "https://res.cloudinary.com/drkkzlx2q/video/upload/v1787537907/Ed_Sheeran_Justin_Bieber_-_I_Don_t_Care_Official_Music_Video.mp3",
+  },
+{
+    id: 4,
+    title: "Closer",
+    artist: "The Chainsmokers",
+    audioUrl: "https://res.cloudinary.com/drkkzlx2q/video/upload/v1787537908/Closer_-_The_Chainsmokers_Lyrics.mp3",
+  },
+{
+    id: 5,
+    title: "Girls Like You",
+    artist: "Maroon 5",
+    audioUrl: "https://res.cloudinary.com/drkkzlx2q/video/upload/v1787537916/Maroon_5_-_Girls_Like_You_ft._Cardi_B_Official_Music_Video.mp3",
+  },
+  {
+    id: 6,
+    title: "Melody",
+    artist: "James Blunt",
+    audioUrl: "https://res.cloudinary.com/drkkzlx2q/video/upload/v1787538412/Lost_Frequencies_ft._James_Blunt_-_Melody_Official_Music_Video.mp3",
+  },
+   {
+    id: 7,
     title: "Jamaican",
     artist: "Afro House",
     audioUrl: "https://res.cloudinary.com/drkkzlx2q/video/upload/v1787335643/HUGEL_x_SOLTO_-_Jamaican_Bam_Bam_Sammy_Flash_Afro_House_Remix.mp3",
   },
+
+  
 
 ];
 
